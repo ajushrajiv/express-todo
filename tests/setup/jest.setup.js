d68f7c822cf16fbc6ccbbd17d677a47fc0c8dbb1
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-03-12 17:24:04 +0100

    test-mutations

--- tests/setup/jest.setup.js
+++ tests/setup/jest.setup.js
@@ -1,5 +1,9 @@
 require("../../src/server");
+const TodoModel = require("../../src/database/models/ToDoModels");
 const todoSequelize = require("../../src/database/setup/database");
+const dotenv = require('dotenv');
+const TestDataTodos = require("./test-data/TestDataTodos");
+dotenv.config({ path: '/../../.test.env' });
 
 module.exports = async () => {
   try {
@@ -8,9 +12,12 @@ module.exports = async () => {
     // });
     
     await todoSequelize.sync();
-    await todoSequelize.dropSchema("Todos");
+    await todoSequelize.dropSchema("todos");
     await todoSequelize.sync();
     // DB mit Daten füllen, um DB auf Test Szenarien vorzubereiten
+    
+    await TodoModel.bulkCreate(TestDataTodos);
+
     console.log("Test DB init");
   } catch (e) {
     console.error("MY DB Issue", e);
---
+++ tests/setup/test-data/TestDataTodos.js
@@ -0,0 +1,28 @@
+const TestDataTodos = [
+    {
+      userId: 1,
+      task: "Schlafen",
+      doBefore: "2024-03-11",
+      completed: true,
+    },
+    {
+      userId: 1,
+      task: "Trinken",
+      doBefore: "2024-03-12",
+      completed: false,
+    },
+    {
+      userId: 1,
+      task: "Essen",
+      doBefore: "2024-03-13",
+      completed: false,
+    },
+    {
+      userId: 1,
+      task: "Joggen",
+      doBefore: "2025-03-12",
+      completed: false,
+    },
+  ];
+
+module.exports = TestDataTodos
